172ceb6ae2e9ae96c84079b9e8ee88f71696aaf5
MichaelGitHub77 <ms970722@gmail.com>
2021-02-23 17:02:26 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  hasil = 0;
+  a = 0;
+  b = 0;
+
+  item = [
+    { nama: "Jeruk", harga: 15000, satuan: "kg" },
+    { nama: "Apel", harga: 18000, satuan: "kg" }
+  ];
+  tulisanOPEN : String;
+  tampil : boolean = true;
+  OPEN(){
+    this.tulisanOPEN = "TOKO DIBUKA" 
+  }
+  values = "";
+  onKey(event: any){
+    this.values += event.target.value +  '|';
+  }
 }
